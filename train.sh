e614fbaf6a6af7310d98a0751728f1549c312cd3
jplumail <jeanplumail76@gmail.com>
2021-09-23 18:33:04 +0200

    install cron in docker

--- train.sh
+++ train.sh
@@ -6,6 +6,8 @@ tensorboard --logdir=$DATA_DIR/logs --host=0.0.0.0 --load_fast=false &
 
 python train.py &
 
+apt-get install cron
+
 crontab -l > sync
 echo "@hourly aws s3 sync /app/data s3://tinyml/data" >> sync
 crontab sync
