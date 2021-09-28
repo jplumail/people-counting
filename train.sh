78492daa81b32545942c6e25c5eed4775e0bae4c
jplumail <jeanplumail76@gmail.com>
2021-09-29 00:01:16 +0200

    new conf

--- train.sh
+++ train.sh
@@ -5,8 +5,8 @@ pip install tensorboard_plugin_profile
 export DATA_DIR=/app/data/
 export LR=0.00001
 
-#export CHECKPOINT=1632694602
-export INITIAL_EPOCH=0
+export CHECKPOINT=1632763181
+export INITIAL_EPOCH=10001
 
 tensorboard --logdir=$DATA_DIR/ShanghaiTechB/logs --host=0.0.0.0 --load_fast=false &
 
